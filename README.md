0cb4d4b36f2551e10947c4455e2a4975426822d2
Enis Bukalo <bukaloenis@gmail.com>
2022-08-05 13:04:25 -0400

    Rework

--- README.md
+++ README.md
@@ -4,14 +4,3 @@
 > From Current Directory.
 
 `cmake --build Server/ && Server/app`
-
----
-
-# To Build Client
-> Run Client Using CMake.
-
-> From Current Directory.
-
-`cmake --build Client/ && Client/app`
-
----
--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string>
 
 int main()
 {
-    std::cout << "HELLO WORLD" << std::endl;
+    // Create socket
+    // Bind socket to IP / port
+    // Mark socket for listening
+    // Accept a call
+    // Close the listening socket
+    // while receiving - display message / echo
+    // Close socket
     return 0;
 };
