bbea5593b7d53a94c975f944f0fb088968d5d1a5
Enis Bukalo <bukaloenis@gmail.com>
2022-08-10 14:44:49 -0400

    Clean up?

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -1,12 +1,4 @@
 #include <iostream>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <string.h>
-#include <string>
-
 #include "SocketServer.h"
 
 int main()
