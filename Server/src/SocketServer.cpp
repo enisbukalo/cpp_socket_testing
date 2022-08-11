dde0c2b12f51b9ee254aa7c3b74259bc4fbbe6e9
Enis Bukalo <bukaloenis@gmail.com>
2022-08-11 09:30:52 -0400

    Minor Comment

--- Server/src/SocketServer.cpp
+++ Server/src/SocketServer.cpp
@@ -19,6 +19,9 @@ void SocketServer::startServer()
 
 int SocketServer::serverLoop()
 {
+    //TODO Handle Errors Coming Back As -1.
+    //TODO How to handle errors back to the main thread?
+    //  This class will be operating in a separate thread.
     std::cout << "Starting Server Loop" << std::endl;
     listeningSocket = createSocket();
     int x = bindSocket();
